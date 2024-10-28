b14a5088f66ceda712a09e13dcd20fce1aafb3f1
bipodboqid <chopin.fantasise.impromptu@gmail.com>
2024-10-28 10:33:33 +0000

    changed model and pipeline definition. not for running Actions yet.

---
+++ uploadFromGitHub/modules/pipeline.py
@@ -0,0 +1,132 @@
+import tensorflow as tf
+from tfx import v1 as tfx
+import kfp
+import tensorflow_model_analysis as tfma
+
+import os
+
+from google.cloud import aiplatform
+from google.cloud.aiplatform import pipeline_jobs
+import logging
+
+from tfx.components import ImportExampleGen, StatisticsGen, SchemaGen, Transform, Trainer
+
+import os
+
+def _create_pipeline(pipeline_name: str, pipeline_root: str, data_root: str,
+                     module_file: str, endpoint_name: str, project_id: str,
+                     region: str) -> tfx.dsl.Pipeline:
+	
+	example_gen = ImportExampleGen(input_base=data_root)
+	
+	statistics_gen = StatisticsGen(examples=example_gen.outputs['examples'])
+	
+	schema_gen = SchemaGen(statistics=statistics_gen.outputs['statistics'],
+						   infer_feature_shape=False)
+	
+	transform = Transform(examples=example_gen.outputs['examples'],
+						  schema=schema_gen.outputs['schema'],
+						  module_file=module_file)
+	
+	trainer = Trainer(
+		module_file=module_file,
+		examples=example_gen.outputs['examples'],
+		schema=schema_gen.outputs['schema'],
+		transform_graph=transform.outputs['transform_graph'],
+		train_args=tfx.proto.TrainArgs(num_steps=100),
+		eval_args=tfx.proto.EvalArgs(num_steps=5))
+	
+	model_resolver = tfx.dsl.Resolver(
+            strategy_class=tfx.dsl.experimental.LatestBlessedModelStrategy,
+            model=tfx.dsl.Channel(type=tfx.types.standard_artifacts.Model),
+            model_blessing=tfx.dsl.Channel(
+                    type=tfx.types.standard_artifacts.ModelBlessing)).with_id(
+            'latest_blessed_model_resolver')
+	
+	eval_config = tfma.EvalConfig(
+            model_specs=[tfma.ModelSpec(label_key='label')],
+            slicing_specs=[
+                    tfma.SlicingSpec(),
+            ],
+            metrics_specs=[
+                    tfma.MetricsSpec(
+                            metrics=[
+                                    tfma.MetricConfig(
+                                            class_name='F1Score',
+                                    )
+                            ],
+                            per_slice_thresholds={
+                                    'F1Score': tfma.PerSliceMetricThresholds(thresholds=[
+                                            tfma.PerSliceMetricThreshold(
+                                                    slicing_specs=[tfma.SlicingSpec()],
+                                                    threshold=tfma.MetricThreshold(
+                                                            value_threshold=tfma.GenericValueThreshold(
+                                                                    lower_bound={'value': 0.6}
+                                                            ),
+                                                            change_threshold=tfma.GenericChangeThreshold(
+                                                                    direction=tfma.MetricDirection.HIGHER_IS_BETTER,
+                                                                    absolute={'value': -1e-10}
+                                                            )
+                                                    )
+                                            )
+                                    ])
+                            }
+                    )
+            ]
+    )
+	evaluator = tfx.components.Evaluator(
+            examples=example_gen.outputs['examples'],
+            model=trainer.outputs['model'],
+            baseline_model=model_resolver.outputs['model'],
+            eval_config=eval_config)
+	
+	# NEW: Configuration for pusher.
+	vertex_serving_spec = {
+		'project_id': project_id,
+		'endpoint_name': endpoint_name,
+		'machine_type': 'n1-standard-4'}
+	
+	serving_image = 'us-docker.pkg.dev/vertex-ai/prediction/tf2-cpu.2-6:latest'
+	
+	pusher = tfx.extensions.google_cloud_ai_platform.Pusher(
+		model=trainer.outputs['model'],
+		model_blessing=evaluator.outputs['blessing'],
+		custom_config={
+			tfx.extensions.google_cloud_ai_platform.ENABLE_VERTEX_KEY:True,
+			tfx.extensions.google_cloud_ai_platform.VERTEX_REGION_KEY:region,
+			tfx.extensions.google_cloud_ai_platform.VERTEX_CONTAINER_IMAGE_URI_KEY:serving_image,
+			tfx.extensions.google_cloud_ai_platform.SERVING_ARGS_KEY:vertex_serving_spec})
+	components = [
+		example_gen,
+		statistics_gen,
+		schema_gen,
+		transform,
+		trainer,
+		model_resolver,
+		evaluator,
+		pusher
+	]
+
+	return tfx.dsl.Pipeline(
+		pipeline_name=pipeline_name,
+		pipeline_root=pipeline_root,
+		components=components)
+
+def save_pipeline_definition(pipeline_name: str, pipeline_root: str, data_root: str,
+					   module_file: str, endpoint_name: str, project_id: str,
+					   region: str, pipeline_definition_file: str) -> str:
+	runner = tfx.orchestration.experimental.KubeflowV2DagRunner(
+		config=tfx.orchestration.experimental.KubeflowV2DagRunnerConfig(),
+		output_filename=pipeline_definition_file)
+
+	_ = runner.run(
+		_create_pipeline(
+			pipeline_name=pipeline_name,
+			pipeline_root=pipeline_root,
+			data_root=data_root,
+			module_file=module_file,
+			endpoint_name=endpoint_name,
+			project_id=project_id,
+			region=region))
+	
+	return pipeline_definition_file
---
+++ uploadFromGitHub/modules/prod_def.py
@@ -0,0 +1,52 @@
+# when test.py finished successfully, 
+# run this module to create pipeline definition for Prod.
+
+if __name__ == "__main__":
+	# record current time to check against definition json's timestamp
+	import time
+	start_time = time.time()
+	
+	import os
+	import glob
+	from google.cloud import aiplatform
+	from google.cloud.aiplatform import pipeline_jobs
+	import pipeline
+	
+	# constants of prod env
+	PROD_GOOGLE_CLOUD_PROJECT = 'mlpipelineportfolio'
+	PROD_GOOGLE_CLOUD_REGION = 'asia-northeast1'
+	PROD_GCS_BUCKET_NAME = 'mlpipelineportfolio_bucket_01'
+	PROD_PIPELINE_NAME = 'pipeline-from-cloudfunction'
+	PROD_PIPELINE_ROOT = 'gs://{}/pipeline_root/{}'.format(PROD_GCS_BUCKET_NAME, PROD_PIPELINE_NAME)
+	PROD_DATA_ROOT = 'gs://mlpipelineportfolio_bucket_01/uploadFromGitHub/tfrecord'
+	PROD_ENDPOINT_NAME = 'prediction-' + PROD_PIPELINE_NAME
+	PROD_PIPELINE_DEFINITION_DIR = '/home/jupyter/SimpleMLPipeline/Mdl01_Model/pipeline-definition/prod/'
+	# PROD_PIPELINE_DEFINITION_FILE = PROD_PIPELINE_DEFINITION_DIR + PROD_PIPELINE_NAME + '_pipeline_' + str(int(start_time)) + '.json'
+	PROD_PIPELINE_DEFINITION_FILE = PROD_PIPELINE_DEFINITION_DIR + PROD_PIPELINE_NAME + '_pipeline.json'
+	PROD_SERVICE_ACCOUNT = 'pj03-vertex-account@mlpipelineportfolio.iam.gserviceaccount.com'
+	PROD_MODULE_FILE = 'gs://mlpipelineportfolio_bucket_01/uploadFromGitHub/modules/utils.py'
+	
+	# remove old pipeline definition jsons
+	files = glob.glob(os.path.join(PROD_PIPELINE_DEFINITION_DIR, '*'))
+	for file in files:
+		try:
+			os.remove(file)
+			print(f"Deleted {file}")
+		except Exception as e:
+			print(f"Couldn't delete {file}. error: {e}")
+	
+	# create and get the path of definition json
+	prod_template_path = pipeline.save_pipeline_definition(pipeline_name=PROD_PIPELINE_NAME,
+														   pipeline_root=PROD_PIPELINE_ROOT,
+														   data_root=PROD_DATA_ROOT,
+														   module_file=PROD_MODULE_FILE,
+														   endpoint_name=PROD_ENDPOINT_NAME,
+														   project_id=PROD_GOOGLE_CLOUD_PROJECT,
+														   region=PROD_GOOGLE_CLOUD_REGION,
+														   pipeline_definition_file=PROD_PIPELINE_DEFINITION_FILE)
+	
+	file_mod_time = os.path.getmtime(prod_template_path)
+	
+	# check json file's timestamp against start time
+	if file_mod_time > start_time:
+		print('Successfully updated/created pipeline definition')
